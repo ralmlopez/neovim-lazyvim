f532f0b981ca400b8f1eccca5632ddba622b5f48
Roberto Lopez <roberto@rhlopez.com>
2024-06-18 19:55:53 -0500

    Add mason ensure

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,18 @@
 return {
   "williamboman/mason.nvim",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, { "prettierd" })
-  end,
+  opts = {
+    ensure_installed = {
+        "css-lsp",
+        "emmet-language-server",
+        "emmet-ls",
+        "html-lsp",
+        "lua-language-server",
+        "omnisharp",
+        "prettierd",
+        "shfmt",
+        "stylua",
+        "typescript-language-server",
+        "vtsls",
+    },
+  }
 }
